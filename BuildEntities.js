539e0086dd98aa4e5d484b4fb4581a56cb905d60
Simon Armstrong <strangemoosey@gmail.com>
2015-06-30 14:25:28 +1000

    bruh

--- BuildEntities.js
+++ BuildEntities.js
@@ -3,11 +3,37 @@ var Rat = new Enemy("Rat", 1, "Rat_Image.png");
 var xSpeed = 0;
 var ySpeed = 0;
 
-var enemySpeedX = Math.floor(Math.random()*Rat.speed + 1);
-var enemySpeedY = Math.floor(Math.random()*Rat.speed + 1);
+var enemySpeedX = 0;
+var enemySpeedY = 0;
 
+
+function EnemyAI()
+{
+	var enemyRandDirect = Math.floor((Math.random() * 4) + 1)
+	switch(enemyRandDirect)
+	{
+		case 1:
+		enemySpeedY = Rat.speed;
+		enemySpeedX = 0;
+		break;
+		case 2:
+		enemySpeedY = -Rat.speed;
+		enemySpeedX = 0;
+		break;
+		case 3:
+		enemySpeedX = Rat.speed;
+		enemySpeedY = 0;
+		break;
+		case 4:
+		enemySpeedX = -Rat.speed;
+		enemySpeedY = 0;
+		break;
+	}
+}
+EnemyAI();
 function BuildEntities(deltaTime)
 {
+	
 	Rat.position.y -= enemySpeedX * deltaTime;
 	Rat.position.x -= enemySpeedY * deltaTime;
 	
