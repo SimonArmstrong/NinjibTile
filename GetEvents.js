546a15551d85cb4a0f3429ef79fba59c97f39cdc
Simon Armstrong <strangemoosey@gmail.com>
2015-06-30 13:47:13 +1000

    hahaha

--- GetEvents.js
+++ GetEvents.js
@@ -11,7 +11,9 @@ function GetEvents()
 	var KEY_S = 83;
 	var KEY_D = 68;
 	var KEY_SHIFT = 16;
-
+	var KEY_ESC = 27;
+	var KEY_ENTER = 13;
+	
 	function KeyDown(e)
 	{
 		switch(e.keyCode)
@@ -40,6 +42,26 @@ function GetEvents()
 				ySpeed = 0;
 				player.direction = 4;
 			break;
+			// Press Space
+			case KEY_SPACE && (gameState === STATE_GAMEOVER):
+				gameState = STATE_LOADING;
+			break;
+			// Press ESC on Game State
+			case KEY_ESC && (gameState === STATE_GAME):
+				gameState = STATE_MENU;
+			break;				
+			// Press ESC on Menu State
+			case KEY_ESC && (gameState === STATE_MENU):
+				gameState = STATE_GAME;			
+			break;			
+			// Press Enter on Game State
+			case KEY_ENTER && (gameState === STATE_GAME):
+				gameState = STATE_SHOP;
+			// Press Enter on Shop State
+			case KEY_ENTER && (gameState === STATE_SHOP):
+					gameState = STATE_GAME;				
+			break;			
+			
 		}
 	}
 	window.addEventListener('keydown', KeyDown);
