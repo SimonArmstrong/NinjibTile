20f1e92551dda25063a1370f2177712b02dbc325
Simon Armstrong <strangemoosey@gmail.com>
2015-06-30 13:50:35 +1000

    Prettied up main.

--- main.js
+++ main.js
@@ -39,34 +39,7 @@ function run()
 	BuildMap();
 	BuildEntities(deltaTime);
 	BuildWalls();
-	BuildShop();
+	//BuildShop();
 }
 
-//-------------------- Don't modify anything below here --------------------------------------------------------------------------------------------
-
-
-// This code will set up the framework so that the 'run' function is called 60 times per second.
-// We have a some options to fall back on in case the browser doesn't support our preferred method.
-(function() {
-  var onEachFrame;
-  if (window.requestAnimationFrame) {
-    onEachFrame = function(cb) {
-      var _cb = function() { cb(); window.requestAnimationFrame(_cb); }
-      _cb();
-    };
-  } else if (window.mozRequestAnimationFrame) {
-    onEachFrame = function(cb) {
-      var _cb = function() { cb(); window.mozRequestAnimationFrame(_cb); }
-      _cb();
-    };
-  } else {
-    onEachFrame = function(cb) {
-      setInterval(cb, 1000 / 60);
-    }
-  }
-  
-  window.onEachFrame = onEachFrame;
-})();
-
-window.onEachFrame(run);
-
+States();
