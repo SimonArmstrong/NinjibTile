6c3d7bacf5d5974b60cdacdd2cf98908a3640c33
Simon Armstrong <strangemoosey@gmail.com>
2015-06-30 13:01:06 +1000

    Walls working

--- BuildWalls.js
+++ BuildWalls.js
@@ -1,4 +1,12 @@
+var WALL_TOP = new Wall(new Vector2(0, 0), new Vector2(800 - 32, 0), "WallTestTile.png");
+var WALL_LEFT = new Wall(new Vector2(0, 0), new Vector2(0, 480), "WallTestTile.png");
+var WALL_RIGHT = new Wall(new Vector2(800 - 32, 0), new Vector2(0, 480), "WallTestTile.png");
+var WALL_BOTTOM = new Wall(new Vector2(0, 480 - 32), new Vector2(800 - 32, 0), "WallTestTile.png");
+
 function BuildWalls()
-{
-	
+{	
+	WALL_TOP.draw();
+	WALL_BOTTOM.draw();
+	WALL_LEFT.draw();
+	WALL_RIGHT.draw();
 }
--- Wall.js
+++ Wall.js
@@ -8,18 +8,14 @@ var Wall = function(vec_p, vec_s, tile)
 Wall.prototype.draw = function()
 {
 	this.tile.src = this.tileImage;
-	if(this.collider.scale.x > 0)
+	
+	for(var x = this.collider.position.x; x <= this.collider.scale.x / 32; x++)
 	{
-		for(var x = this.collider.position.x; x <= this.collider.scale.x / 32; x++)
-		{
-			context.drawImage(this.tile, this.collider.position.x + (32 * x), 0);
-		}
+		context.drawImage(this.tile, this.collider.position.x + (32 * x), this.collider.position.y);
 	}
-	if(this.collider.scale.y > 0)
+	
+	for(var y = this.collider.position.y; y <= this.collider.scale.y / 32; y++)
 	{
-		for(var y = this.collider.position.y; y <= this.collider.scale.y / 32; y++)
-		{
-			context.drawImage(this.tile, 0, this.collider.position.y + (32 * y));
-		}
+		context.drawImage(this.tile, this.collider.position.x, this.collider.position.y + (32 * y));
 	}
 }
