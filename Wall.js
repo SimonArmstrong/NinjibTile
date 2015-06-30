f537794a7a35e57d9a28c1c8d3a572402d73dae3
Simon Armstrong <strangemoosey@gmail.com>
2015-06-30 11:15:00 +1000

    Added Wall editor class.

--- Wall.js
+++ Wall.js
@@ -7,14 +7,19 @@ var Wall = function(vec_p, vec_s, tile)
 
 Wall.prototype.draw = function()
 {
-	this.tile.src = tileImage;
-	for(var x = this.collider.position.x; x <= this.collider.scale.x / 32; x++)
+	this.tile.src = this.tileImage;
+	if(this.collider.scale.x > 0)
 	{
-		context.drawImage(tile, this.collider.position.x + (32 * i), 0);
+		for(var x = this.collider.position.x; x <= this.collider.scale.x / 32; x++)
+		{
+			context.drawImage(this.tile, this.collider.position.x + (32 * x), 0);
+		}
 	}
-	
-	for(var y = this.collider.position.y; y <= this.collider.scale.y / 32; y++)
+	if(this.collider.scale.y > 0)
 	{
-		context.drawImage(tile, 0, this.collider.position.y + (32 * i));
+		for(var y = this.collider.position.y; y <= this.collider.scale.y / 32; y++)
+		{
+			context.drawImage(this.tile, 0, this.collider.position.y + (32 * y));
+		}
 	}
 }
