e8084945b38f291d7febaaade29ebf5b349c8b79
Simon Armstrong <strangemoosey@gmail.com>
2015-06-30 11:35:12 +1000

    Added Base Game States

--- main.js
+++ main.js
@@ -29,8 +29,12 @@ function getDeltaTime()
 
 //-------------------- Don't modify anything above here-----------------------------------------------------------------------------------------------
 
-var player = new Player("test_player.png");
-var Rat = new Enemy ("Rat", 1, "Rat_Image.png");
+var STATE_MENU = 0;
+var STATE_GAME = 1;
+
+var gameState = STATE_MENU;
+
+var Rat = new Enemy("Rat", 1, "Rat_Image.png");
 
 var mapImage = document.createElement("img");
 mapImage.src = "mapDay.png";
@@ -112,49 +116,61 @@ var FACE_UP;
 var FACE_DOWN;
 var FACE_LEFT;
 var FACE_RIGHT;
-
-
+var secondstowait = 3;
 function run()
 {
 	var deltaTime = getDeltaTime();
-	
-	player.position.y -= ySpeed * deltaTime;
-	player.position.x -= xSpeed * deltaTime;
-	
-	FACE_UP = new Collider("player", new Vector2(player.scale.x, 1), player.position);
-	FACE_DOWN = new Collider("player", new Vector2(player.scale.x, 1), new Vector2(0, player.position.y + player.scale.y));
-	FACE_LEFT = new Collider("player", new Vector2(1, player.scale.y), new Vector2(player.position.y + player.scale.y, 0));
-	FACE_RIGHT = new Collider("player", new Vector2(1, player.scale.y), new Vector2(player.position.x + player.scale.x, player.position.y));
-	
-	if(FACE_UP.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
-	{
-		console.log("UP");
+	if(gameState === STATE_GAME)
+	{	
+		player.position.y -= ySpeed * deltaTime;
+		player.position.x -= xSpeed * deltaTime;
+		
+		FACE_UP = new Collider("player", new Vector2(player.scale.x, 1), player.position);
+		FACE_DOWN = new Collider("player", new Vector2(player.scale.x, 1), new Vector2(0, player.position.y + player.scale.y));
+		FACE_LEFT = new Collider("player", new Vector2(1, player.scale.y), new Vector2(player.position.y + player.scale.y, 0));
+		FACE_RIGHT = new Collider("player", new Vector2(1, player.scale.y), new Vector2(player.position.x + player.scale.x, player.position.y));
+		
+		if(FACE_UP.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
+		{
+			console.log("UP");
+		}
+		if(FACE_DOWN.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
+		{
+			console.log("DOWN");
+		}
+		if(FACE_LEFT.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
+		{
+			console.log("LEFT");
+		}
+		if(FACE_RIGHT.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
+		{
+			console.log("RIGHT");
+		}
+		
+		context.drawImage(mapImage, 0, 0);
+		
+		context.fillStyle = "#000";
+		context.fillRect(canvas.width - 32, 0, 32, canvas.height);
+		
+		Rat.draw();
+		player.draw();
+		
 	}
-	if(FACE_DOWN.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
+	else if(gameState === STATE_MENU)
 	{
-		console.log("DOWN");
+		secondstowait -= deltaTime;
+		
+		context.fillStyle = "#fff";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		
+		context.fillStyle = "#000";
+		context.fillText(secondstowait, 10, 10);
+		
+		if(secondstowait <= 0)
+		{
+			gameState = STATE_GAME;
+		}
 	}
-	if(FACE_LEFT.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
-	{
-		console.log("LEFT");
-	}
-	if(FACE_RIGHT.isTouching(new Collider("Wall", new Vector2(32, canvas.height), new Vector2(canvas.width - 32, 0))) === true)
-	{
-		console.log("RIGHT");
-	}
-	
-	context.drawImage(mapImage, 0, 0);
-	
-	context.fillStyle = "#000";
-	context.fillRect(canvas.width - 32, 0, 32, canvas.height);
-	
-	Rat.draw();
-	player.draw();
-	
-	
-	
-	//map.drawMap(0, 0);
-	//UI.draw();
 }
 
 //-------------------- Don't modify anything below here --------------------------------------------------------------------------------------------
