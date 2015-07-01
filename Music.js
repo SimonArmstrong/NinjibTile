35bf7b28d8b65f85d373f7c1629e4f9cf92308ef
Simon Armstrong <strangemoosey@gmail.com>
2015-07-01 14:46:52 +1000

    my update

---
+++ Music.js
@@ -0,0 +1,18 @@
+//
+//var musicBackground;
+
+
+
+function initialize()
+{
+	//musicBackground = new Howl
+	(
+		{
+			urls: ["Castlevania-II-Simons-Quest-NES-Music-Final-Battle-Last-Boss.mp3"]
+			loop: true,
+			buffer: true,
+			volume: 0.5
+		} 
+	);	
+	musicBackground.play();
+}
