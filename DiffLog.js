ad3c4024d35d924628d61a9a478f193e37314433
Simon Armstrong <strangemoosey@gmail.com>
2015-07-03 12:41:35 +1000

    Diff log - Brendan

---
+++ DiffLog.js
@@ -0,0 +1,55 @@
+
+var DiffLog = function(i)
+{		
+	var ppx = player.position.x;
+	var ppy = player.position.y;
+	var epx = enemies[i].position.x;
+	var epy = enemies[i].position.y;
+	
+	var upleft = false;
+	var upright = false;
+	var downleft = false;
+	var downright = false;
+
+
+	/*
+	var positions = [ppx, ppy, epx, epy];
+
+	for(var j = 0; j <= positions.length - 1; j++)
+	{
+		if(positions[j] < 0)
+	}
+	*/
+	
+	var posXDiff = ppx - epx
+	var posYDiff = ppy - epy
+	
+	var logToCon = false;
+	
+	if(logToCon = true)
+	{
+		if(posXDiff > 0)
+		{
+			if(posYDiff > 0)
+			{
+				console.log("The enemy is above and left of the player");
+			}
+			if(posYDiff < 0)
+			{
+				console.log("The enemy is below and left of the player");
+			}
+
+		}
+		if(posXDiff < 0)
+		{
+			if(posYDiff > 0)
+			{
+				console.log("The player is below and left of the enemy");
+			}
+			if(posYDiff < 0)
+			{
+				console.log("The player is above and left of the enemy");
+			}
+		}
+	}
+};
